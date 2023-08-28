cb034833cfc2dac24e0fab193c8885232b4f7be7
LIvanoff <drfrily@gmail.com>
2023-08-29 00:35:11 +0300

    update unet.py

--- unet.py
+++ unet.py
@@ -6,7 +6,7 @@ from models import UNetTransposed, UNetMaxUnpool
 
 class UNet(nn.Module):
     def __init__(self,
-                 pooling: str = None,
+                 pooling: str,
                  activ: str = None,
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,17 +18,17 @@ class UNet(nn.Module):
                 self.backbone = UNetTransposed()
             else:
                 raise NotImplementedError(f'Pooling {pooling} not implemented.\n'
-                                          f'You can use follow activation functions: maxpooling and transposed')
+                                          f'You can use follow activation functions: maxpooling or transposed')
 
         if activ is not None:
             self.is_activ = True
             if activ == 'sigmoid':
-                self.activ = torch.nn.Sigmoid()
+                self.activ = nn.Sigmoid()
             elif activ == 'softmax':
                 self.activ = nn.Softmax(dim=1)
             else:
                 raise NotImplementedError(f'Activation function {activ} not implemented.\n'
-                                          f'You can use follow activation functions: sigmoid and softmax')
+                                          f'You can use follow activation functions: sigmoid or softmax')
 
     def forward(self, x):
         out = self.backbone(x)
